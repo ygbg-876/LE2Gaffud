3a593cac13153b2854b67973860417f5b245ee3c
ygbg-876 <ygbgaffud@mymail.mapua.edu.ph>
2026-09-09 14:52:39 +0800

    Add files via upload

---
+++ gaffudLE2/BlogDataLibrary/Database/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogDataLibrary.Database
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName, bool isStoredProcedure);
+        void SaveData<T>(string sqlStatement, T parameters, string connectionStringName, bool isStoredProcedure);
+    }
+}
---
+++ gaffudLE2/BlogDataLibrary/Database/SqlDataAccess.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace BlogDataLibrary.Database
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement,
+            U parameters,
+            string connectionStringName,
+            bool isStoredProcedure)
+        {
+            CommandType commandType = CommandType.Text;
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters,
+                commandType: commandType).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement,
+            T parameters,
+            string connectionStringName,
+            bool isStoredProcedure)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters, commandType: commandType);
+            }
+        }
+    }
+}
